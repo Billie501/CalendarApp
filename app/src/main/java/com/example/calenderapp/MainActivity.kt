f5b449b4f931a37312cc5a5bdbaeb7807ce6fe14
Billie501 <billiejean.laing@gmail.com>
2024-02-06 11:07:44 +1300

    End product. A perfectly working calendar using Kotlin - not Java

--- app/src/main/java/com/example/calenderapp/MainActivity.kt
+++ app/src/main/java/com/example/calenderapp/MainActivity.kt
@@ -5,49 +5,78 @@ import android.os.Bundle
 import android.view.View
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.time.YearMonth
 
-public class MainActivity extends AppCompatActivity
+class MainActivity : AppCompatActivity(), CalendarAdapter.OnItemListener {
+    private lateinit var monthYearText: TextView
+    private lateinit var calendarRecyclerView: RecyclerView
+    private lateinit var selectedDate: LocalDate
 
-{
-    private TextView monthYearText;
-    private RecyclerView calendarRecyclerView;
-    private LocalDate selectedDate;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    initWidgets();
-    selectedDate = LocalDate.now();
-    setMonthView();
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initWidgets()
+        selectedDate = LocalDate.now()
+        setMonthView()
     }
 
-    private void initWidgets()
-    {
-       calendarRecyclerView = findViewbyId(R.id.calendarRecyclerView);
-        monthYearText = findViewById(R.id.monthYearTV);
+    private fun initWidgets() {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView)
+        monthYearText = findViewById(R.id.monthYearTV)
     }
 
-    private void setMonthView()
-    {
-        monthYearText.setText(monthYearFromDate(selectedDate));
-        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+    private fun setMonthView() {
+        monthYearText.text = monthYearFromDate(selectedDate)
+        val daysInMonth = daysInMonthArray(selectedDate)
+
+        val calendarAdapter = CalendarAdapter(daysInMonth, this)
+        val layoutManager = GridLayoutManager(applicationContext, 7)
+        calendarRecyclerView.layoutManager = layoutManager
+        calendarRecyclerView.adapter = calendarAdapter
     }
 
-    private String monthYearFromDate(LocalDate date)
-    {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
-        return date.format(formatter);
+    private fun daysInMonthArray(date: LocalDate): ArrayList<String> {
+        val daysInMonthArray = ArrayList<String>()
+        val yearMonth = YearMonth.from(date)
+
+        val daysInMonth = yearMonth.lengthOfMonth()
+
+        val firstOfMonth = selectedDate.withDayOfMonth(1)
+        val dayOfWeek = firstOfMonth.dayOfWeek.value
+
+        for (i in 1..42) {
+            if (i <= dayOfWeek || i > daysInMonth + dayOfWeek) {
+                daysInMonthArray.add("")
+            } else {
+                daysInMonthArray.add((i - dayOfWeek).toString())
+            }
+        }
+        return daysInMonthArray
     }
 
-    public void previousMonthAction(View view)
-    {
-
+    private fun monthYearFromDate(date: LocalDate): String {
+        val formatter = DateTimeFormatter.ofPattern("MMMM yyyy")
+        return date.format(formatter)
     }
 
-    public void nextMonthAction(View view)
-    {
+    fun previousMonthAction(view: View) {
+        selectedDate = selectedDate.minusMonths(1)
+        setMonthView()
+    }
 
+    fun nextMonthAction(view: View) {
+        selectedDate = selectedDate.plusMonths(1)
+        setMonthView()
+    }
+
+    override fun onItemClick(position: Int, dayText: String) {
+        if (dayText == "") {
+            val message = "Selected Date $dayText ${monthYearFromDate(selectedDate)}"
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        }
     }
 }
