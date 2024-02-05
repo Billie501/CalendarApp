a682bb56ad10bc9e93d30e48cd3a3929a3cc108a
Billie501 <billiejean.laing@gmail.com>
2024-02-06 10:02:41 +1300

    Created a Java code MainActivity to use instead of the MainActivity Kotlin

--- app/src/main/java/com/example/calenderapp/MainActivity.kt
+++ app/src/main/java/com/example/calenderapp/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.calenderapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 public class MainActivity extends AppCompatActivity
+
 {
     private TextView monthYearText;
     private RecyclerView calendarRecyclerView;
@@ -13,15 +16,38 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        initWidgets();
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    initWidgets();
+    selectedDate = LocalDate.now();
+    setMonthView();
     }
 
     private void initWidgets()
     {
-        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+       calendarRecyclerView = findViewbyId(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
     }
 
-    fun previousMonthAction(view: View) {}
+    private void setMonthView()
+    {
+        monthYearText.setText(monthYearFromDate(selectedDate));
+        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+    }
+
+    private String monthYearFromDate(LocalDate date)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        return date.format(formatter);
+    }
+
+    public void previousMonthAction(View view)
+    {
+
+    }
+
+    public void nextMonthAction(View view)
+    {
+
+    }
 }
