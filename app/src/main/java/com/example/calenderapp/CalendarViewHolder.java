9a21b03841ce9c9e5ad79621a066b8fc11a73296
Billie501 <billiejean.laing@gmail.com>
2024-02-05 16:54:23 +1300

    Created an Array List and a Constructor for daysOfMonth as well as set up a layout inflater with layout parameters for Calendar View Holder

--- app/src/main/java/com/example/calenderapp/CalendarViewHolder.java
+++ app/src/main/java/com/example/calenderapp/CalendarViewHolder.java
@@ -1,4 +1,17 @@
 package com.example.calenderapp;
 
-public class CalendarViewHolder {
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CalendarViewHolder extends RecyclerView.ViewHolder {
+
+    private final TextView dayOfMonth;
+    public CalendarViewHolder(@NonNull View itemView)
+    {
+        super(itemView);
+        dayOfMonth = itemView.findViewById(R.id.cellDayText);
+    }
 }
